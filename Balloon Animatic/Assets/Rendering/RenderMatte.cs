d7c0ba44f82816052e5a0293b14790bfc2bf8caa
Kaitlyn <kand8998@gmail.com>
2019-11-06 13:08:41 -0700

    File Changes

---
+++ Balloon Animatic/Assets/Rendering/RenderMatte.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RenderMatte : MonoBehaviour
+{
+    public BoolData useMatte;
+    public Material materialObj;
+    void Start()
+    {
+        if (useMatte.value)
+        {
+            GetComponent<Renderer>().material = materialObj;
+        }
+    }
+
+}
